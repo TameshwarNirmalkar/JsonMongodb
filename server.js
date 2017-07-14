0d4a4ff9cc9c3f4ae1e7231ca72598d4d32cff87
Tameshwar Nirmalkar <tameshwar.nirmalkar@mastercard.com>
2017-07-14 11:01:06 +0530

    remove name

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-/**
- * Created by e062110 on 6/17/16.
- */
-
 var express = require('express');
 var env = process.env.NODE_ENV || 'development';
-
 var app = express();
-
-
-
-var config = require('./server/config/config.js');
+var config = require('./server/config/config');
 
 require('./server/config/express')(app, config, env);
 
 app.listen(config.port, function () {
     console.log('Express server listening on port http://localhost:' + config.port);
-    console.log('NODE_ENV = ' + env +
-        '\n__dirname = ' + __dirname +
-        '\nprocess.cwd = ' + process.cwd());
+    console.log('NODE_ENV = ' , env ,
+        '\n__dirname = ' , __dirname ,
+        '\nprocess.cwd = ' , process.cwd());
 });
